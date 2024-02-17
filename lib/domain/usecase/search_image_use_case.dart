75ffa2f3161ca9ff018b5b609400f7338165b4aa
김상현 <shkim>
2024-02-17 18:03:52 +0900

    - 잘못된 파일명 수정

--- lib/domain/usecase/search_image_use_case.dart
+++ lib/domain/usecase/search_image_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:search_image/data/remote/api/repository/api_repository_impl.dart';
 import 'package:search_image/domain/entity/search_image_result_entity.dart';
-import 'package:search_image/domain/enums/SortType.dart';
+import 'package:search_image/domain/enums/sort_type.dart';
 
 import 'package:search_image/domain/repository/api_repository.dart';
 
